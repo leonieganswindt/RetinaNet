97a434234c8c10a1b92d0931310c548778e6fbd4
Leonie Ganswindt <lg@neleso.com>
2019-07-01 15:11:26 +0200

    Change evaluation

--- evaluation/cocoeval.py
+++ evaluation/cocoeval.py
@@ -2,6 +2,7 @@
 
 """
 import sys
+import os
 import argparse
 import numpy as np
 import matplotlib
@@ -9,13 +10,17 @@ matplotlib.use('Agg')
 sys.path.append('../')
 import pycocotools.coco as COCO
 import pycocotools.cocoeval as COCOeval
+from cfgs import config as cfg
+import os
 
 
 def coco_bbox_eval(result_file, annotation_file):
 
     ann_type = 'bbox'
     coco_gt = COCO.COCO(annotation_file)
+    #print('COCO_gt: ', coco_gt)
     coco_dt = coco_gt.loadRes(result_file)
+    print('COCO_dt: ', coco_dt)
     cocoevaler = COCOeval.COCOeval(coco_gt, coco_dt, ann_type)
     cocoevaler.evaluate()
     cocoevaler.accumulate()
@@ -35,19 +40,25 @@ def coco_proposal_eval(result_file, annotation_file):
 
 
 if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--result', type=str, default='', help='detection result file')
-    parser.add_argument('-a', '--annotation', type=str,
-                        default='/public_datasets/COCO/annotations/instances_val2017.json',
-                        help='COCO groundtruth')
+    parser.add_argument('-r', '--result', type=str, default='./result.json', help='detection result file')
+    parser.add_argument('-a', '--annotation', type=str, default='val', help='COCO groundtruth')
     parser.add_argument('-t', '--type', type=str, default='bbox', help='eval type: [bbox, seg, proposal]')
     _args = parser.parse_args()
 
+    config = cfg.config['coco_baseline']
+
+    filex = 'annotations/instances_' + _args.annotation + '.json'
+    annotation_file = os.path.join(config['data_dir'], filex)
+
+    print('Result File used: ', _args.result)
+
     if _args.type == 'bbox':
-        coco_bbox_eval(_args.result, _args.annotation)
+        coco_bbox_eval(_args.result, annotation_file)
     elif _args.type == 'proposal':
-        coco_proposal_eval(_args.result, _args.annotation)
+        coco_proposal_eval(_args.result, annotation_file)
     else:
         pass
 
