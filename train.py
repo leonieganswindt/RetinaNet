c3548734431115bded480b3401ab601aaf251f45
Leonie Ganswindt <lg@neleso.com>
2019-07-01 15:09:54 +0200

    modify for microscopic images

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import os
 import tqdm
 import argparse
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from models import retina
@@ -19,7 +20,12 @@ from torch.nn.utils import clip_grad
 import tensorboardX
 from utils import logger
 from cfgs import config as cfg
+import tensorflow as tf
+from torchvision.utils import make_grid
 
+import matplotlib.pyplot as plt
+
+import datetime
 
 def initialize(config, args):
 
@@ -30,7 +36,8 @@ def initialize(config, args):
         os.mkdir(os.path.join(logdir, args.experiment))
 
     model_dump_dir = os.path.join(logdir, args.experiment, 'model_dump')
-    tb_dump = os.path.join(logdir, args.experiment, 'tb_dump')
+    #tb_dump = os.path.join(logdir, args.experiment, 'tb_dump')
+    tb_dump = config['tb_dump_dir']
 
     if not os.path.exists(model_dump_dir):
         os.mkdir(model_dump_dir)
@@ -38,7 +45,7 @@ def initialize(config, args):
     if not os.path.exists(tb_dump):
         os.mkdir(tb_dump)
 
-    config['tb_dump_dir'] = tb_dump
+    #config['tb_dump_dir'] = tb_dump
     config['model_dump_dir'] = model_dump_dir
 
 
@@ -67,7 +74,7 @@ def train(args, config):
     model = nn.DataParallel(model, device_ids=list(range(args.device)))
 
     if args.dataset == 'COCO':
-        train_dataset = coco.COCODetection(dataroot=config['data_dir'], imageset='train2017', config=config)
+        train_dataset = coco.COCODetection(dataroot=config['data_dir'], imageset='train', config=config, training=True)
     elif args.dataset == 'VOC':
         train_dataset = voc.VOC2012(dataroot=config['data_dir'], imageset='train', config=config)
     else:
@@ -111,11 +118,19 @@ def train(args, config):
         pbar = tqdm.tqdm(range(len(train_loader)))
         for i in pbar:
             lr = learning_rate_decay(optimizer, global_step, config)
-            img, labels, boxes = next(data_iter)
+            img, labels, boxes, anchors = next(data_iter)
+
+            #add image to tensorboard
+            tbimg = make_grid(img)
+            #writer.add_images('img', img, epoch)
+
+            #writer.add_image_with_boxes('img with BBox', img[0], boxes[0], epoch)
+
             img = img.cuda()
             labels = labels.long().cuda()
             boxes = boxes.cuda()
             cls_outputs, bbox_outputs = model(img)
+
             cls_loss = cls_criterion(cls_outputs, labels)
             box_loss = box_criterion(bbox_outputs, boxes, labels)
             loss = cls_loss + box_loss
@@ -133,7 +148,38 @@ def train(args, config):
             ))
             losses.append(loss.item())
 
+            scores = cls_outputs
+            max_score, max_labels = torch.max(scores[0], dim=1)
+
+
+            """
+            good_anchors = []
+            for cls in range(config['num_classes'] - 1):
+
+                # filter predictions through 'classification threshold'
+                score = scores[:, cls]
+                cls_inds = score > config['cls_thresh']
+                # current class has the max score over all classes
+                max_inds = max_labels == cls
+                cls_inds = max_inds * cls_inds
+                if cls_inds.sum() < 1:
+                    continue
+
+                # _boxes [K, 4]
+                good_anchors.append(anchors[cls_inds])
+
+            good_anchors = torch.cat(good_anchors, dim=0)
+            """
+
+            good_anchors = anchors[0][max_score > config['cls_thresh']]
+
             # learning rate decay
+
+        if epoch % 25 == 0:
+            writer.add_images('img', img, epoch)
+            writer.add_image_with_boxes('img with gt BBox', img[0], anchors[0][labels[0]>0], epoch)
+            # TODO: plot only good boxes
+            writer.add_image_with_boxes('img with predicted BBox', img[0], good_anchors, epoch)
 
         print("e:{} loss: {}".format(epoch, np.mean(losses)))
         logger.save_checkpoints(model.module, optimizer, epoch, global_step,
@@ -142,16 +188,19 @@ def train(args, config):
 
 
 if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--device', type=int, default=1, help='training with ? GPUs')
     parser.add_argument('-b', '--batch_size', type=int, default=4, help='training batch size per GPU')
     parser.add_argument('-c', '--continue_path', type=str, default='', help='continue model parameters')
-    parser.add_argument('-e', '--experiment', type=str, default='voc_baseline',
+    parser.add_argument('-e', '--experiment', type=str, default='coco_baseline',
                         help='experiment name, correspond to `config.py`')
-    parser.add_argument('-ds', '--dataset', type=str, default='VOC', help='dataset')
+    parser.add_argument('-ds', '--dataset', type=str, default='COCO', help='dataset')
 
     _args = parser.parse_args()
     config = cfg.config[_args.experiment]
     initialize(config, _args)
     train(_args, config)
+
+    print('Finished at: ', datetime.datetime.now().time())
